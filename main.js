53b6bf4fbb8e11199122845d54608d1842e8079a
Zachery Cox <zachc1x@gmail.com>
2017-06-08 15:56:03 -0400

    booya

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // Add your JS here.
+var newtodo=document.querySelector(".new-todo");
+var todolist=document.querySelector(".todo-list");
+newtodo.addEventListener("keypress",
+function(event) {
+var timer = setTimeout(function() {
+if (event.keyCode===13){
+var todoTwo=event.target.value;
+event.target.value="";
+var newTodoLi=document.createElement("li");
+newTodoLi.textContent=todoTwo;
+todolist.appendChild(newTodoLi);
+    };
+  }, 200);
+return event;
+});
